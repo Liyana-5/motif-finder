9c455cc6e99b1460bd0d3c6f2c5a5be2aba9f768
Liyana Saleem <Liyanaps17@gmail.com>
2025-10-24 00:48:21 +0100

    git restore --staged PDB5_solutions.py
    ddded a print statement
    
    

---
+++ finding_motif_fasta_txt.py
@@ -0,0 +1,8 @@
+# Finding motifs in a FASTA file
+'''Here we are given a fasta file containing amino acid sequences from Trypanosoma brucei 'Tgondii_Proteins.fasta' and txt file 'Tgondii_product_descriptions.txt
+containing gene id and product description.
+
+We are to find all occurrences of a given motif in these sequences and then obtain the 
+        gene id , product description and the last 30 amino acid sequences and write it to another output file '''
+        
+print("Program to find motifs in a FASTA file and extract relevant information.")
