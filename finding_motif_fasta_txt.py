d6a9292cb0bcda66e40b2f61a363571763704e87
Liyana Saleem <Liyanaps17@gmail.com>
2025-10-24 19:15:49 +0100

    Updater version 1 with try and exception

--- finding_motif_fasta_txt.py
+++ finding_motif_fasta_txt.py
@@ -9,6 +9,7 @@ print("Program to find motifs in a FASTA file and extract relevant information."
 
 # import necessary libraries
 import re
+import sys
 
 des_file = "Tgondii_product_descriptions.txt"
 fasta_file = "Tgondii_Proteins.fasta"
@@ -18,31 +19,41 @@ motif = r'Y..[YFT].{0,6}$' # Define the motif pattern
 
 # Read product descriptions into a dictionary
 product_dict = {}
-with open(des_file, 'r') as df:
-    next(df)
-    for line in df:
-        parts = line.strip().split('\t')
-        gene_id = parts[0]
-        description = parts[2]
-        product_dict[gene_id] = description
+try:
+    with open(des_file, 'r') as df:
+        next(df)  # Skip header line
+        for line in df:
+            parts = line.strip().split('\t')
+            gene_id = parts[0]
+            description = parts[2]
+            product_dict[gene_id] = description
+except Exception as e:
+    print(f"Error reading {des_file}: {e}")
+    sys.exit(1)
+
+
 
 # Read the FASTA file 
 # Write the information to the output file
-with open(output_file, 'w') as of:
-    of.write("Gene ID\tProduct Description\tLast 30 Amino Acids\n")
-    with open(fasta_file) as ff:
-        for line in ff:
-            if line.startswith('>'):
-                gene = line.rstrip().lstrip('>')
-                seq = next(ff).rstrip()
-            match = re.search(motif, seq)
-            if match:
-                seq_id = gene
-                out_seq = seq[len(seq)-30:]
-                assert len(out_seq) == 30
-                match_seq = match.group()
-                out_seq_lower = out_seq.replace(match_seq, match_seq.lower())
-                description = product_dict.get(seq_id, "Unknown")
-                of.write(f"{seq_id}\t{description}\t{out_seq_lower}\n")
+try:
+    with open(output_file, 'w') as of:
+        of.write("Gene ID\tProduct Description\tLast 30 Amino Acids\n")
+        with open(fasta_file) as ff:
+            for line in ff:
+                if line.startswith('>'):
+                    gene = line.rstrip().lstrip('>')
+                    seq = next(ff).rstrip()
+                match = re.search(motif, seq)
+                if match:
+                    seq_id = gene
+                    out_seq = seq[len(seq)-30:]
+                    assert len(out_seq) == 30
+                    match_seq = match.group()
+                    out_seq_lower = out_seq.replace(match_seq, match_seq.lower())
+                    description = product_dict.get(seq_id, "Unknown")
+                    of.write(f"{seq_id}\t{description}\t{out_seq_lower}\n")
+except Exception as e:
+    print(f"Erro processing files: {e}")
+    sys.exit(1)
 
 print(f"Motif search complete. Results written to {output_file}.")
